949519f7366d13b6cc0f0acd86a9894674c8cfa2
HenryPaik1 <renwenxue0@gmail.com>
2019-06-06 13:16:31 +0900

    separate main.ty and key_securiy.tf to be clear

---
+++ key_security.tf
@@ -0,0 +1,34 @@
+provider "aws"{
+    region = "ap-northeast-2"
+}
+
+resource "aws_key_pair" "dss_key" {
+key_name = "dss_key"
+public_key = "${file("~/.ssh/dss_key.pub")}"
+}
+
+resource "aws_security_group" "dss-terraform" {
+name = "security_dss"
+description = "security group"
+
+ingress {
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+from_port = 8888
+to_port = 8888
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+}
--- main.tf
+++ main.tf
@@ -2,85 +2,28 @@
 # DEPLOY AN INSTANCE, THEN TRIGGER A PROVISIONER
 # See test/terraform_ssh_example.go for how to write automated tests for this code.
 # ---------------------------------------------------------------------------------------------------------------------
-
+ 
 provider "aws" {
 region = "ap-northeast-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
 }
 
 data "aws_security_group" "default" {
 name = "default"
 }
 
-data "aws_security_group" "security_dss" {
-name = "security_dss"
-}
-
-
-resource "aws_key_pair" "dss_key" {
-key_name = "dss_key"
-public_key = "${file("~/.ssh/dss_key.pub")}"
-}
-
-resource "aws_security_group" "dss-terraform" {
-name = "security_dss"
-description = "dss group"
-
-ingress {
-from_port = 22
-to_port = 22
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
-
-ingress {
-from_port = 80
-to_port = 80
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
-
-ingress {
-from_port = 8888
-to_port = 8888
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
-}
-
-
 resource "aws_instance" "article_clf" {
   ami                    = "ami-06e7b9c5e0c4dd014"
   instance_type          = "t2.micro"
-  vpc_security_group = ["${aws_security_group.security_dss.id}"]
+  vpc_security_group_ids = ["${aws_security_group.security_dss.id}",
+                         "${data.aws_security_group.default.id}"]
   key_name               = "dss_key"
 
-  # This EC2 Instance has a public IP and will be accessible directly from the public Internet
-  associate_public_ip_address = true
-
   tags = {
-    Name = "{var.instance_name}"
+    Name = "${var.instance_name}"
   }
   
-}
-
-
-
-resource "aws_security_group" "article_clf" {
-  name = "${var.instance_name}"
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = "${var.ssh_port}"
-    to_port   = "${var.ssh_port}"
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -95,9 +38,10 @@ resource "null_resource" "clf_null" {
   connection {
     type = "ssh"
     host = "${aws_instance.article_clf.public_ip}"
+    public_key = "${file("~/.ssh/dss_key.pub")}"
     user = "${var.ssh_user}"
     port = "${var.ssh_port}"
-    agent = true
+    timeout = "1m"
   }
 
    provisioner "remote-exec" {
@@ -105,10 +49,11 @@ resource "null_resource" "clf_null" {
                             "sudo apt-get update",
                             "sudo apt-get -y install python3-pip",
                             "sudo apt-get -y install unzip",
+                            "wget https://chromedriver.storage.googleapis.com/74.0.3729.6/chromedriver_linux64.zip"
                             "unzip chromedriver_linux64.zip",
                             "git clone https://github.com/HenryPaik1/article_classification.git",
-                            "python3 run_article.py 0"]
-                            
-
- 
+                            "python3 ~/article_classification/run_article.py 0
+                            ]
+                           }
+    }
 }
